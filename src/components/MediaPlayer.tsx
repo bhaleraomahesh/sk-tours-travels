813a23667d7b0615467df14a3ed98915438c8d51
Mahesh Bhalerao <mahesh@binaryhat.in>
2024-11-30 08:25:35 +0530

    video autoplay not happening on mobile

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -10,43 +10,43 @@ const MediaPlayer: React.FC<IMediaPlayerProps> = (props: IMediaPlayerProps) => {
   const theme = useTheme();
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    // Create an intersection observer callback function
-    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        // Check if the video is in the viewport
-        if (entry.isIntersecting) {
-          // Play the video when it's in view
-          if (videoRef.current) {
-            videoRef.current.play();
-          }
-        } else {
-          // Pause the video when it's out of view
-          if (videoRef.current) {
-            videoRef.current.pause();
-          }
-        }
-      });
-    };
+  // useEffect(() => {
+  //   // Create an intersection observer callback function
+  //   const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+  //     entries.forEach((entry) => {
+  //       // Check if the video is in the viewport
+  //       if (entry.isIntersecting) {
+  //         // Play the video when it's in view
+  //         if (videoRef.current) {
+  //           videoRef.current.play();
+  //         }
+  //       } else {
+  //         // Pause the video when it's out of view
+  //         if (videoRef.current) {
+  //           videoRef.current.pause();
+  //         }
+  //       }
+  //     });
+  //   };
 
-    // Create an IntersectionObserver instance with the callback
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null, // Observe intersections with the viewport
-      threshold: 0.4, // 50% of the video should be in view to trigger
-    });
+  //   // Create an IntersectionObserver instance with the callback
+  //   const observer = new IntersectionObserver(handleIntersection, {
+  //     root: null, // Observe intersections with the viewport
+  //     threshold: 0.4, // 50% of the video should be in view to trigger
+  //   });
 
-    // Observe the video element
-    if (videoRef.current) {
-      observer.observe(videoRef.current);
-    }
+  //   // Observe the video element
+  //   if (videoRef.current) {
+  //     observer.observe(videoRef.current);
+  //   }
 
-    // Cleanup the observer on component unmount
-    return () => {
-      if (videoRef.current) {
-        observer.unobserve(videoRef?.current);
-      }
-    };
-  }, []); // Empty array means this effect runs onc
+  //   // Cleanup the observer on component unmount
+  //   return () => {
+  //     if (videoRef.current) {
+  //       observer.unobserve(videoRef?.current);
+  //     }
+  //   };
+  // }, []); // Empty array means this effect runs onc
 
   return (
     <Box
@@ -67,7 +67,8 @@ const MediaPlayer: React.FC<IMediaPlayerProps> = (props: IMediaPlayerProps) => {
         <CardMedia
           ref={videoRef}
           component="video"
-          controls
+          autoPlay
+          loop
           muted
           sx={{
             width: "100%",
@@ -76,8 +77,6 @@ const MediaPlayer: React.FC<IMediaPlayerProps> = (props: IMediaPlayerProps) => {
             mb: theme.spacing(1),
             borderRadius: 0,
           }}
-          autoPlay
-          loop
         >
           <source src={mediaURL} type="video/mp4" />
         </CardMedia>
