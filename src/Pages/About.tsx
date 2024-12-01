cd3c11759a46d8608557fcdf62002ead02586944
Mahesh Bhalerao <mahesh@binaryhat.in>
2024-12-01 21:24:13 +0530

    updated about us text

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -34,24 +34,20 @@ const About = forwardRef<HTMLDivElement, object>((_, ref) => {
         <Box component="section" sx={{ width: "100%", height: "100vh" }}>
           <Typography variant="h3">About Us</Typography>
           <Typography>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum. It is a long
-            established fact that a reader will be distracted by the readable
-            content of a page when looking at its layout. The point of using
-            Lorem Ipsum is that it has a more-or-less normal distribution of
-            letters, as opposed to using 'Content here, content here', making it
-            look like readable English. Many desktop publishing packages and web
-            page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their
-            infancy. Various versions have evolved over the years, sometimes by
-            accident, sometimes on purpose (injected humour and the like).
+            SK Tours & Travels - Your Trusted Travel Partner With over 10 years
+            of experience in the transport industry, SK Tours & Travels has been
+            delivering reliable and safe cab services to customers across the
+            region. Having completed number of trips, we pride ourselves on
+            punctuality, safety, and customer satisfaction. Our customers trust
+            us for our consistent, high-quality service, whether it's for
+            business or leisure travel. We have built strong relationships not
+            only with individual travelers but also with travel agencies and
+            corporate clients. Our transport services extend to IT companies,
+            hotel trips, and other professional travel needs. At SK Tours &
+            Travels, your journey is in safe hands, and we ensure every trip is
+            hassle-free and comfortable. Choose SK Tours & Travels for all your
+            transportation needs – where reliability meets customer
+            satisfaction!
           </Typography>
         </Box>
       </Paper>
